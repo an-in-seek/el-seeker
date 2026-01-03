67bcdc8b0b99af56ee7e3dae53bf80e00a13075e
an-in-seek <dktmrorl@gmail.com>
2026-01-03 20:38:30 +0900

    refactor: BibleDictionaryApiController -> DictionaryApiController 네이밍 변경

--- src/main/kotlin/com/elseeker/bible/presentation/api/DictionaryApiController.kt
+++ src/main/kotlin/com/elseeker/bible/presentation/api/DictionaryApiController.kt
@@ -4,15 +4,11 @@ import com.elseeker.bible.application.bible.service.BibleDictionaryService
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/study/dictionaries")
-class BibleDictionaryApiController(
+class DictionaryApiController(
     private val bibleDictionaryService: BibleDictionaryService
 ) {
 
@@ -21,11 +17,11 @@ class BibleDictionaryApiController(
         @RequestParam(required = false) keyword: String?,
         @RequestParam(defaultValue = "0") page: Int,
         @RequestParam(defaultValue = "10") size: Int
-    ): ResponseEntity<BibleDictionaryApiResponse.DictionarySliceResponse> {
+    ): ResponseEntity<DictionaryApiResponse.DictionarySliceResponse> {
         val pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "term"))
         val dictionaryPage = bibleDictionaryService.getDictionaries(keyword, pageable)
-        val response = BibleDictionaryApiResponse.DictionarySliceResponse(
-            content = dictionaryPage.content.map(BibleDictionaryApiResponse.DictionaryItem::from),
+        val response = DictionaryApiResponse.DictionarySliceResponse(
+            content = dictionaryPage.content.map(DictionaryApiResponse.DictionaryItem::from),
             hasNext = dictionaryPage.hasNext(),
             totalCount = dictionaryPage.totalElements
         )
@@ -35,8 +31,8 @@ class BibleDictionaryApiController(
     @GetMapping("/{id}")
     fun getDictionary(
         @PathVariable id: Long
-    ): ResponseEntity<BibleDictionaryApiResponse.DictionaryDetail> {
-        val response = BibleDictionaryApiResponse.DictionaryDetail.from(
+    ): ResponseEntity<DictionaryApiResponse.DictionaryDetail> {
+        val response = DictionaryApiResponse.DictionaryDetail.from(
             bibleDictionaryService.getDictionary(id)
         )
         return ResponseEntity.ok(response)
